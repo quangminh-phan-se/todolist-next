990fc5ca18688fe070b58dc05817b0a2617dde51
thao <leanhthao01@gmail.com>
2025-07-30 00:23:16 +0700

    feat: add sidebar content and install navigation menu

--- src/data-seed/sidebar-data.ts
+++ src/data-seed/sidebar-data.ts
@@ -1,6 +1,13 @@
 import {
   IconChecklist,
   IconLayoutDashboard,
+  IconPackages,
+  IconMessages,
+  IconUsers,
+  IconLockAccess,
+  IconBug,
+  IconSettings,
+  IconHelp
 } from '@tabler/icons-react'
 import { type SidebarData } from "@/types/sidebar-data";
 
@@ -20,11 +27,74 @@ export const sidebarData: SidebarData = {
           icon: IconLayoutDashboard,
         },
         {
-          title: 'Todo',
+          title: 'Tasks',
           url: '/todo',
           icon: IconChecklist,
+        },
+        {
+          title: 'Apps',
+          url: '/todo',
+          icon: IconPackages,
+        },
+        {
+          title: 'Chats',
+          url: '/todo',
+          icon: IconMessages,
+        },
+        {
+          title: 'Users',
+          url: '/todo',
+          icon: IconUsers,
+        },
+        {
+          title: 'Secured By Clerk',
+          icon: IconChecklist,
+          items: [
+            {
+              title: 'Sign In',
+              url: '/todo'
+            },
+            {
+              title: 'Sign Up',
+              url: '/todo'
+            },
+            {
+              title: 'User Management',
+              url: '/todo'
+            }
+          ]
         }
       ],
     },
+    {
+      title: 'Pages',
+      items: [
+        {
+          title: 'Auth',
+          url: '/todo',
+          icon: IconLockAccess,
+        },
+        {
+          title: 'Errors',
+          url: '/todo',
+          icon: IconBug,
+        }
+      ]
+    },
+    {
+      title: 'Other',
+      items: [
+        {
+          title: 'Setting',
+          url: '/todo',
+          icon: IconSettings,
+        },
+        {
+          title: 'Help Center',
+          url: '/todo',
+          icon: IconHelp,
+        }
+      ]
+    }
   ],
 }
